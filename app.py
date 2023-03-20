60d4e2baa280f4721bcd2253e9f31800789a1aaf
Zack Cheng <65567443+zacktcheng@users.noreply.github.com>
2023-03-20 16:14:57 -0700

    Add /db_create route.

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
     conn = psycopg2.connect("postgres://lab_10_zacktcheng_user:UDmYPCIYtHhRaVmjOXKZ17xsBPPaRqQq@dpg-cgcd2l1mbg55nqinqk30-a/lab_10_zacktcheng");
     conn.close();
     return 'Database Connection Successful';
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgres://lab_10_zacktcheng_user:UDmYPCIYtHhRaVmjOXKZ17xsBPPaRqQq@dpg-cgcd2l1mbg55nqinqk30-a/lab_10_zacktcheng");
+    cur = conn.cursor();
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''');
+    conn.commit();
+    conn.close();
+    return 'Basketball Table Successfully Created';
