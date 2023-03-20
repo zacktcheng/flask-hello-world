d9978a25dab2967fde5b03d513933e8753897259
Zack Cheng <65567443+zacktcheng@users.noreply.github.com>
2023-03-20 14:43:36 -0700

    Add postgre db testing route

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+import psycopg2
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://lab_10_zacktcheng_user:UDmYPCIYtHhRaVmjOXKZ17xsBPPaRqQq@dpg-cgcd2l1mbg55nqinqk30-a/lab_10_zacktcheng");
+    conn.close();
+    return 'Database Connection Successful';
