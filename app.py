cbc687f8f0990f1ce2cab0b533a4c98ac6d43ed8
Zack Cheng <65567443+zacktcheng@users.noreply.github.com>
2023-03-20 19:04:15 -0700

    Add db_insert

--- app.py
+++ app.py
@@ -29,3 +29,19 @@ def creating():
     conn.commit();
     conn.close();
     return 'Basketball Table Successfully Created';
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgres://lab_10_zacktcheng_user:UDmYPCIYtHhRaVmjOXKZ17xsBPPaRqQq@dpg-cgcd2l1mbg55nqinqk30-a/lab_10_zacktcheng");
+    cur = conn.cursor();
+    cur.execute('''
+    INSERT INTO Basketball (First, Last, City, Name, Number)
+    Values
+    ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+    ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+    ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+    ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''');
+    conn.commit();
+    conn.close();
+    return 'Basketball Table Successfully Populated';
