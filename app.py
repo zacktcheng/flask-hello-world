aa39aa5ee39ec2ad757495c4f3c56cdb575e2b17
Zack Cheng <65567443+zacktcheng@users.noreply.github.com>
2023-03-20 19:34:16 -0700

    Add db_select

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def selecting():
     cur.execute('''
     SELECT * FROM Basketball;
     ''');
-    records = curr.fetchall();
+    records = cur.fetchall();
     conn.close();
     response_string = '';
     response_string += '<table>';
